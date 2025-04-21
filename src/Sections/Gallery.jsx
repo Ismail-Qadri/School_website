60c33672601a72582ebf934659af1f9d561e4665
Ismail <ismailqadri98@gmail.com>
2025-04-21 15:24:08 +0530

    completed

--- src/Sections/Gallery.jsx
+++ src/Sections/Gallery.jsx
@@ -34,7 +34,7 @@ import img29 from "../assets/gallery/g29.jpeg";
 import img30 from "../assets/gallery/g30.jpeg";
 
 
-const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img22, img23, img24, img25, img26, img27, img28, img29,img30];
+const images = [img1, img2, img4, img5, img6, img7, img8, img9, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29,img30];
 const Gallery = () => {
   const [selectedImage, setSelectedImage] = useState(null);
 
